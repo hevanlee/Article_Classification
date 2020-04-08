65845b591cbfe1cc09bcc2df2511d90f60ef3190
Evan Lee <evan.lee7007@gmail.com>
2020-04-09 00:16:24 +1000

    SVM done, convergence error

---
+++ linearSVM.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+
+df = pd.read_csv("training.csv")
+
+data = df["article_words"]
+topics = df["topic"]
+
+# Create bag of words
+count = CountVectorizer()
+bag_of_words = count.fit_transform(data)
+
+X = bag_of_words
+
+labels = []
+for topic in topics:
+    if topic == 'ARTS CULTURE ENTERTAINMENT':
+        labels.append(1)
+    elif topic == 'BIOGRAPHIES PERSONALITIES PEOPLE':
+        labels.append(2)
+    elif topic == 'DEFENCE':
+        labels.append(3)
+    elif topic == 'DOMESTIC MARKETS':
+        labels.append(4)
+    elif topic == 'FOREX MARKETS':
+        labels.append(5)
+    elif topic == 'HEALTH':
+        labels.append(6)
+    elif topic == 'MONEY MARKETS':
+        labels.append(7)
+    elif topic == 'SCIENCE AND TECHNOLOGY':
+        labels.append(8)
+    elif topic == 'SHARE LISTINGS':
+        labels.append(9)
+    elif topic == 'SPORTS':
+        labels.append(10)
+    else: 
+        labels.append(0)
+
+df['labels'] = labels
+
+# Create target vector
+y = labels
+
+# Divide training set into training (9000) and development (500) sets
+X_train = X[:9000]
+X_test = X[9000:]
+y_train = y[:9000]
+y_test = y[9000:]
+
+# TODO: Create new unseen test instances
+# test1 = count.transform([''])
+# test2 = count.transform([''])
+
+clf = LinearSVC(max_iter = 1000)
+model = clf.fit(X_train, y_train)
+
+predicted_y = model.predict(X_test)
+
+print('Accuracy score:', accuracy_score(y_test, predicted_y))
+print('Precision score:', precision_score(y_test, predicted_y, average=None, zero_division=0))
+print('Recall score:', recall_score(y_test, predicted_y, average=None, zero_division=0))
+
+print(classification_report(y_test, predicted_y))
