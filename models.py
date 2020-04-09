c96cb1ec435bcb41c26f8bfe25e98688085c460f
Evan Lee <evan.lee7007@gmail.com>
2020-04-09 21:22:33 +1000

    added all models to a single file

---
+++ models.py
@@ -0,0 +1,89 @@
+import pandas as pd
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+
+df = pd.read_csv("training.csv")
+
+data = df["article_words"]
+topics = df["topic"]
+
+labels = []
+for topic in topics:
+    if topic == 'ARTS CULTURE ENTERTAINMENT':
+        labels.append(1)
+    elif topic == 'BIOGRAPHIES PERSONALITIES PEOPLE':
+        labels.append(2)
+    elif topic == 'DEFENCE':
+        labels.append(3)
+    elif topic == 'DOMESTIC MARKETS':
+        labels.append(4)
+    elif topic == 'FOREX MARKETS':
+        labels.append(5)
+    elif topic == 'HEALTH':
+        labels.append(6)
+    elif topic == 'MONEY MARKETS':
+        labels.append(7)
+    elif topic == 'SCIENCE AND TECHNOLOGY':
+        labels.append(8)
+    elif topic == 'SHARE LISTINGS':
+        labels.append(9)
+    elif topic == 'SPORTS':
+        labels.append(10)
+    else: 
+        labels.append(0)
+
+df['labels'] = labels
+
+# Create target vector
+y = labels
+
+# Divide training set into training (9000) and development (500) sets
+X_train = data[:9000]
+X_test = data[9000:]
+y_train = y[:9000]
+y_test = y[9000:]
+
+# Fit to training set
+count = CountVectorizer()
+X_train_count = count.fit_transform(X_train)
+
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_count)
+
+X_test_counts = count.transform(X_test)
+X_test_tfidf = tfidf_transformer.transform(X_test_counts)
+
+names = ["K Nearest Neighbours", "Bernoulli Naive Bayes",
+"Multinomial Naive Bayes", "Linear SVM",
+"Decision Tree Classifier", "Random Forest Classifier", 
+"AdaBoost Classifier", "Multiple Layer Perceptron"]
+
+classifiers = [
+    KNeighborsClassifier(),
+    BernoulliNB(),
+    MultinomialNB(),
+    SGDClassifier(),
+    DecisionTreeClassifier(),
+    RandomForestClassifier(),
+    AdaBoostClassifier(),
+    MLPClassifier()
+]
+
+for name, clf in zip(names, classifiers):
+    print("--" + name)
+    model = clf.fit(X_train_tfidf, y_train)
+    predicted_y = model.predict(X_test_tfidf)
+
+    print('Accuracy score:', accuracy_score(y_test, predicted_y))
+    print('Precision score:', precision_score(y_test, predicted_y, average=None, zero_division=0))
+    print('Recall score:', recall_score(y_test, predicted_y, average=None, zero_division=0))
+
+    print(classification_report(y_test, predicted_y))
+
+
